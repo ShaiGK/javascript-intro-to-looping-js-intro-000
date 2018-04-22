e299d3677d044c29f0ccd13daee1804942c25968
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-04-22 01:43:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,7 @@
 function forLoop (array) {
   for (let i = 0; i < 25; i++) {
     array.push ( "I am " + i + " strange loop." );
+    console.log ("I am " + i + " strange loop.");
+    return "I am " + i + " strange loop."
   }
 }
